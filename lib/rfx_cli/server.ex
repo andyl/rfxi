4007c43cdb0537eda0976592958ee4e69d3f26da
andyl <andy@r210.com>
2021-06-20 18:28:10 -0700

    Add start message

--- lib/rfx_cli/server.ex
+++ lib/rfx_cli/server.ex
@@ -2,6 +2,7 @@ defmodule RfxCli.Server do
   def start() do
     # We configure the endpoint with `server: true`,
     # so it's gonna start listening
+    IO.puts("STARTING SERVER")
     case Application.ensure_all_started(:rfxi) do
       {:ok, _} -> :ok
       {:error, _} -> :error
