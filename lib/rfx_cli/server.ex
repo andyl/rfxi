38d2329e98a2cbf5c17b86645dac9ae9ec18f4f9
andyl <andy@r210.com>
2021-06-22 07:10:44 -0700

    Add find_port function

--- lib/rfx_cli/server.ex
+++ lib/rfx_cli/server.ex
@@ -15,4 +15,15 @@ defmodule RfxCli.Server do
     args = Application.get_env(:rfxi, RfxiWeb.Endpoint) ++ [server: true]
     Application.put_env(:rfxi, RfxiWeb.Endpoint, args)
   end
+
+  # to be used by the CLI - send notifications the server if it is active
+  def find_port(port) do
+    case :gen_tcp.listen(port, []) do
+      {:ok, socket} ->
+        :gen_tcp.close(socket)
+        port
+      {:error, :eaddrinuse} ->
+        find_port(port + 1)
+    end
+  end
 end
