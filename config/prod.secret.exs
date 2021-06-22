b4b9d1bdf9162956f6042ad67aa7353fab2f49c7
andyl <andy@r210.com>
2021-06-21 18:57:23 -0700

    Update config to start server from CLI

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,19 +4,14 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+skey = "123456789 123456789 123456789 123456789 123456789 123456789 123456789"
 
 config :rfxi, RfxiWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "8082"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: skey
 
 # ## Using releases (Elixir v1.9+)
 #
--- lib/rfx_cli/server.ex
+++ lib/rfx_cli/server.ex
@@ -2,10 +2,17 @@ defmodule RfxCli.Server do
   def start() do
     # We configure the endpoint with `server: true`,
     # so it's gonna start listening
+    Logger.configure(level: :debug)
+    set_config()
     IO.puts("STARTING SERVER")
-    case Application.ensure_all_started(:rfxi) do
-      {:ok, _} -> :ok
-      {:error, _} -> :error
-    end
+    IO.puts("Ctrl-C to exit...")
+    IO.inspect(Application.stop(:rfxi))
+    IO.inspect(Rfxi.Application.start())
+    Process.sleep(:infinity)
+  end
+
+  defp set_config do
+    args = Application.get_env(:rfxi, RfxiWeb.Endpoint) ++ [server: true]
+    Application.put_env(:rfxi, RfxiWeb.Endpoint, args)
   end
 end
