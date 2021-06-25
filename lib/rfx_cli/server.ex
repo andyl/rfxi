08efae6c63372bc6bd744943ca6d8acd9ecdd2b0
andyl <andy@r210.com>
2021-06-24 17:09:07 -0700

    Delete old server code

--- lib/rfx_cli/server.ex
+++ lib/rfx_cli/server.ex
@@ -10,7 +10,8 @@ defmodule RfxCli.Server do
   def start() do
     # We configure the endpoint with `server: true`,
     # so it's gonna start listening
-    Logger.configure(level: :debug)
+    # Logger.configure(level: :debug)
+    Logger.configure(level: :info)
     set_config()
     IO.puts("STARTING SERVER")
     IO.puts("Ctrl-C to exit...")
