06af0765813809eb75cf15edfe1404cb841d37e0
andyl <andy@r210.com>
2021-07-04 13:11:54 -0700

    Fix repl exit error message

--- lib/rfx_cli/credo_repl.ex
+++ lib/rfx_cli/credo_repl.ex
@@ -1,5 +1,16 @@
 defmodule RfxCli.CredoRepl do
 
+  @moduledoc """
+  Credo REPL
+
+  Commands:
+    - help                   - display a help message
+    - suggest                - run and display a credo analysis
+    - detail <CredoIssueId>  - show the detail for a credo issue
+    - apply <RfxOperationId> - apply an operation to the filesystem
+    - exit                   - quit the repl
+  """
+
   def start do
     loop("begin", %{count: 1})
   end
@@ -17,28 +28,45 @@ defmodule RfxCli.CredoRepl do
   end
 
   def loop(_, state) do
-    command = get_input(state.count)
+    command = get_input(state)
 
-    case run(command) do
+    case run(command, state) do
       :halt -> IO.puts("DONE")
-      _ -> loop("continue", %{state | count: state.count + 1})
+      new_state -> loop("continue", %{new_state | count: state.count + 1})
     end
   end
 
-  defp get_input(count) do
-    IO.gets("rfx_credo_repl_#{count}> ")
-    |> String.trim()
+  defp get_input(state) do
+    IO.gets("rfx_credo_repl_#{state.count}> ")
+    |> String.trim() |> String.split(" ")
   end
 
-  defp run("") do
-    :no_cmd
+  defp run([""], state) do
+    state
   end
 
-  defp run("exit") do
+  defp run(["help"], state) do 
+    state
+  end
+
+  defp run(["detail", _issue_id], state) do 
+    state
+  end
+
+  defp run(["apply", _operation_id], state) do 
+    state
+  end
+
+  defp run(["suggest"], state) do 
+    results = RfxCli.Util.Credo.run()
+    %{ state | credo_results: results }
+  end
+
+  defp run(["exit"], _state) do
     :halt
   end
 
-  defp run(command) do
-    RfxCli.Base.main(command)
-  end
+  # defp run(command, state) do
+  #   RfxCli.Base.main(command)
+  # end
 end
